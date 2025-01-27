0346676b695410d754f2bf1b3f5e997ee2d8bdcb
ier24 <ryohr93@gmail.com>
2025-01-27 21:44:32 +0900

    feat: 予約代理エージェントの実装

--- src/app/agentConfigs/index.ts
+++ src/app/agentConfigs/index.ts
@@ -2,11 +2,13 @@ import { AllAgentConfigsType } from "@/app/types";
 import frontDeskAuthentication from "./frontDeskAuthentication";
 import customerServiceRetail from "./customerServiceRetail";
 import simpleExample from "./simpleExample";
+import restaurantReservation from "./restaurantReservation";
 
 export const allAgentSets: AllAgentConfigsType = {
   frontDeskAuthentication,
   customerServiceRetail,
   simpleExample,
+  restaurantReservation,
 };
 
 export const defaultAgentSetKey = "simpleExample";
